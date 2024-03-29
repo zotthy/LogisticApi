94e99dfc83cb22bc07dc71b4bd448e22f025e9a6
zotthy <se05110800@gmail.com>
2024-03-29 10:34:37 +0100

    addregister

--- src/main/java/logistic/apilogistic/config/SecurityConfig.java
+++ src/main/java/logistic/apilogistic/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,22 +25,24 @@ class SecurityConfig {
     MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
         return new MvcRequestMatcher.Builder(introspector);
     }
-
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http,
-                                           MvcRequestMatcher.Builder mvc,
-                                           AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        AuthenticationManager authenticationManager = authenticationManagerBuilder.getOrBuild();
-        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtService);
+                                           MvcRequestMatcher.Builder mvc) throws Exception {
         BearerTokenFilter bearerTokenFilter = new BearerTokenFilter(jwtService);
         http.authorizeHttpRequests(requests -> requests
-                .requestMatchers(mvc.pattern("/auth")).permitAll()
                 .requestMatchers(mvc.pattern("/register")).permitAll()
+                .requestMatchers(mvc.pattern("/login")).permitAll()
+                .requestMatchers(mvc.pattern("/hi")).hasRole("ADMIN")
+                .requestMatchers(mvc.pattern("/hiuser")).hasRole("ADMIN")
                 .anyRequest().permitAll()
         );
         http.sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.csrf(csrfCustomizer -> csrfCustomizer.disable());
-        http.addFilterBefore(jwtAuthenticationFilter, AuthorizationFilter.class);
         http.addFilterBefore(bearerTokenFilter, AuthorizationFilter.class);
         return http.build();
     }
--- src/main/java/logistic/apilogistic/controllers/registerController.java
+++ src/main/java/logistic/apilogistic/controllers/registerController.java
@@ -1,21 +1,50 @@
 package logistic.apilogistic.controllers;
 
+import logistic.apilogistic.authRequest.LoginRequest;
+import logistic.apilogistic.config.JwtService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class registerController {
     private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
-    public registerController(AuthenticationManager authenticationManager) {
+    public registerController(AuthenticationManager authenticationManager, JwtService jwtService) {
         this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
     }
 
-    @GetMapping()
-    String getinfo(@RequestHeader("Authorization") String token) {
-        System.out.println(token);
-        return "hello";
+    @PostMapping("/login")
+    ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()));
+
+        List<String> authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = jwtService.createSignedJWT(authentication.getName(),authorities);
+        return new ResponseEntity<>(token, HttpStatus.OK);
+    }
+
+    @GetMapping("/hi")
+    String hi(@RequestHeader("Authorization") String token){
+        return token;
     }
 }
