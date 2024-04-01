5e361f95ccfde128dc75875cefe24fe9589f4be7
zotthy <se05110800@gmail.com>
2024-04-01 17:30:38 +0200

    fds

--- src/main/java/logistic/apilogistic/repository/CargoHandlerRepository.java
+++ src/main/java/logistic/apilogistic/repository/CargoHandlerRepository.java
@@ -2,6 +2,8 @@ package logistic.apilogistic.repository;
 
 import logistic.apilogistic.entity.Cargo_handler;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CargoHandlerRepository extends JpaRepository<Cargo_handler,Long> {
 }
--- src/main/java/logistic/apilogistic/repository/CargoOwnersRepository.java
+++ src/main/java/logistic/apilogistic/repository/CargoOwnersRepository.java
@@ -2,12 +2,12 @@ package logistic.apilogistic.repository;
 
 import logistic.apilogistic.entity.Cargo_owners;
 import logistic.apilogistic.entity.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CargoOwnersRepository extends JpaRepository<Cargo_owners,Long> {
     List<Cargo_owners> findByUser(User user);
 }
